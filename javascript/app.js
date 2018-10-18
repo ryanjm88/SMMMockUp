d86f27674ce88cab4db2c9102d859279eae46944
Ryan McKenzie <ryanjm88@gmail.com>
2018-10-18 16:21:02 -0400

    donate backup

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,14 @@
 $('.carousel').carousel({
     interval: 2000
   });
+
+$("#donateButton").on("click", function(event)  {
+  event.preventDefault();
+  window.location.href="content/donate.html";
+});
+
+$("#donateButton2").on("click", function(event)  {
+  event.preventDefault();
+  window.location.href="./donate.html";
+  console.log("this");
+});
