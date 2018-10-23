ab461b0e8109d5e163d102101811f1479d9a256a
Ryan McKenzie <ryanjm88@gmail.com>
2018-10-22 23:53:26 -0400

    programs and partners pages work

--- javascript/app.js
+++ javascript/app.js
@@ -12,3 +12,9 @@ $("#donateButton2").on("click", function(event)  {
   window.location.href="./donate.html";
   console.log("test");
 });
+
+$("#donateNow").on("click", function(event) {
+  event.preventDefault();
+  window.location.href="https://www.paypal.com/donate/?token=HnvFCNh4fZmRv5PuIng4gp_Gids10UbcRrL1KC2PEcgMRGAMcSt_gjckiBeEvLZ_QUncKG&country.x=US&locale.x=";
+  window.open(url, '_blank');
+});
