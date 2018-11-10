f843ef557465d29225248f64720a5e185febd3be
Ryan McKenzie <ryanjm88@gmail.com>
2018-11-10 14:44:37 -0500

    navbar button and HTML updates

--- javascript/app.js
+++ javascript/app.js
@@ -6,15 +6,3 @@ $("#donateButton").on("click", function(event)  {
   event.preventDefault();
   window.location.href="content/donate.html";
 });
-
-$("#donateButton2").on("click", function(event)  {
-  event.preventDefault();
-  window.location.href="./donate.html";
-  console.log("test");
-});
-
-$("#donateNow").on("click", function(event) {
-  event.preventDefault();
-  window.location.href="https://www.paypal.com/donate/?token=HnvFCNh4fZmRv5PuIng4gp_Gids10UbcRrL1KC2PEcgMRGAMcSt_gjckiBeEvLZ_QUncKG&country.x=US&locale.x=";
-  window.open(url, '_blank');
-});
